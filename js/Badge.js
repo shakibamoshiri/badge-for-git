12556271e828b26a8fd1cbd7ccecc0e0e093302d
Shakbia Moshiri <shakiba.moshiri@yahoo.com>
2018-04-19 14:12:54 +0430

    Badge.js: remove 10 of styles

--- js/Badge.js
+++ js/Badge.js
@@ -28,7 +28,7 @@ function Badge()
     // no matter
     this.style = 0;
 
-    // for style: 1, 4, 5, 7, 9, 10, 11, 12, 15, 16, 17, 18 and 19
+    // for style: 1, 3, 5, 6, 7, 8, 9
     this.delimiter = 0;
 }
 
@@ -41,6 +41,9 @@ Badge.prototype.div = function( class_name )
 
     this.max_suffix = 0;
     this.max_width = 0;
+
+    this.text_offset = this.char_width;
+    this.offset      = this.char_width / 2;
 
     var div = document.createElement( 'DIV' );
     div.className = class_name;
@@ -56,6 +59,7 @@ Badge.prototype.init = function()
     this.offset      = this.char_width / 2;
 }
 
+// prefix:suffix
 Badge.prototype.style_1 = function( text )
 {
     var M = 'M' + this.begin + ',0';
@@ -79,98 +83,8 @@ Badge.prototype.style_1 = function( text )
     return [ M, L1, L2, L3 ].join( ' ' );
 }
 
+// prefix><suffix
 Badge.prototype.style_2 = function( text )
-{
-    var M = 'M' + this.begin + ',0';
-    this.text_x = this.tip + this.text_offset;
-
-    this.end = this.end + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-    this.max_width = this.end;
-    var L1 = ( this.end + this.offset  * 2 ) + ',0';
-
-    var L2 = ( this.end - this.offset  ) + ',' + this.char_height;
-
-    var L3 = ( this.begin - this.rescue  ) + ',' + this.char_height;
-
-    this.begin = this.end + this.offset;
-    this.tip = this.end;
-    this.rescue = this.offset * 2;
-    this.offset = 0;
-
-    return [ M, L1, L2, L3 ].join( ' ' );
-}
-
-Badge.prototype.style_3 = function( text )
-{
-    var M = 'M' + this.begin + ',0';
-    this.text_x = this.tip + this.text_offset;
-
-    this.end = this.end + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-    this.max_width = this.end;
-    var L1 = ( this.end - this.offset ) + ',0';
-
-    var L2 = ( this.end + this.offset * 2  ) + ',' + this.char_height;
-
-    var L3 = this.begin + this.rescue +  ',' + this.char_height;
-
-    this.begin = this.end - this.offset ;
-    this.tip = this.end;
-    this.rescue = this.offset * 2;
-    this.offset = 0;
-
-    return [ M, L1, L2, L3 ].join( ' ' );
-}
-
-Badge.prototype.style_4 = function( text )
-{
-    var M = 'M' + this.begin + ',' + this.char_height;
-    this.text_x = this.tip + this.text_offset * 2;
-
-    this.end = this.begin + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-    this.end += this.text_offset;
-    var L1 = this.end + ',' + this.char_height;
-
-    var L2 = ( this.end + this.offset * 3 ) + ',0';
-
-    var L3 = ( this.begin + this.offset * 3 ) + ',0';
-
-    if( this.delimiter !== 0 && --this.max_suffix )
-    {
-        this.end += this.delimiter;
-    }
-
-    this.begin = this.end;
-    this.tip = this.begin;
-    this.max_width = this.end + ( this.offset * 3 );
-
-    return [ M, L1, L2, L3 ].join( ' ' );
-}
-
-Badge.prototype.style_5 = function( text )
-{
-    var M = 'M' + this.begin + ',0';
-    this.text_x = this.tip + this.text_offset * 2;
-
-    this.end = this.begin + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-    this.end += this.text_offset;
-    var L1 = this.end + ',0';
-
-    var L2 = ( this.end + this.offset * 3 ) + ',' + this.char_height;
-
-    var L3 = ( this.begin + this.offset * 3 ) + ',' + this.char_height;
-
-    if( this.delimiter !== 0 && --this.max_suffix )
-    {
-        this.end += this.delimiter;
-    }
-
-    this.begin = this.end;
-    this.tip = this.begin;
-    this.max_width = this.end + ( this.offset * 3 );
-    return [ M, L1, L2, L3 ].join( ' ' );
-}
-
-Badge.prototype.style_6 = function( text )
 {
     var M = 'M' + this.begin + ',0';
     this.text_x = this.tip + this.text_offset;
@@ -196,7 +110,8 @@ Badge.prototype.style_6 = function( text )
     return [ M, L1, L2, L3, L4, L5 ].join( ' ' );
 }
 
-Badge.prototype.style_7 = function( text )
+// <prefix>:<suffix>
+Badge.prototype.style_3 = function( text )
 {
     var M = 'M' + this.tip + ',' + ( this.char_height / 2 );
     this.text_x = this.tip + this.text_offset + this.offset;
@@ -236,7 +151,8 @@ Badge.prototype.style_7 = function( text )
     return [ M, L1, L2, L3, L4, L5 ].join( ' ' );
 }
 
-Badge.prototype.style_8 = function( text )
+// <prefix:suffix>
+Badge.prototype.style_4 = function( text )
 {
     var M = 'M' + this.tip + ',' + ( this.char_height / 2 );
     this.text_x = this.begin + this.text_offset + this.offset;
@@ -262,7 +178,8 @@ Badge.prototype.style_8 = function( text )
     return [ M, L1, L2, L3, L4, L5 ].join( ' ' );
 }
 
-Badge.prototype.style_9 = function( text )
+// >prefix>:>suffix>
+Badge.prototype.style_5 = function( text )
 {
     var M = 'M' + this.begin + ',0';
     this.text_x = this.begin + this.text_offset + this.offset;
@@ -290,7 +207,8 @@ Badge.prototype.style_9 = function( text )
     return [ M, L1, L2, L3, L4, L5 ].join( ' ' );
 }
 
-Badge.prototype.style_10 = function( text )
+// <prefix<:<suffix<
+Badge.prototype.style_6 = function( text )
 {
     var M = 'M' + this.tip + ',' + ( this.char_height / 2 );
     this.text_x = this.begin + this.text_offset + this.offset;
@@ -323,7 +241,8 @@ Badge.prototype.style_10 = function( text )
     return [ M, L1, L2, L3, L4, L5, L6 ].join( ' ' );
 }
 
-Badge.prototype.style_11 = function( text )
+// >prefix<:>suffix<
+Badge.prototype.style_7 = function( text )
 {
     var M = 'M' + this.begin + ',0';
     this.text_x = this.begin + this.text_offset + this.offset;
@@ -355,7 +274,8 @@ Badge.prototype.style_11 = function( text )
     return [ M, L1, L2, L3, L4, L5 ].join( ' ' );
 }
 
-Badge.prototype.style_12 = function( text )
+// >prefix<:<>:>suffix<
+Badge.prototype.style_8 = function( text )
 {
     var M = 'M' + this.begin + ',0';
     this.text_x = this.begin + this.text_offset + this.offset;
@@ -414,109 +334,8 @@ Badge.prototype.style_12 = function( text )
     return [ M, L1, L2, L3, L4, L5 ].join( ' ' );
 }
 
-Badge.prototype.style_13 = function( text )
-{
-    var M = 'M' + this.begin + ',' + this.char_height;
-    this.text_x = this.tip + this.text_offset + this.offset;
-
-    this.end = this.begin + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-    this.max_width = this.end;
-    var L1 = this.end + ',' + this.char_height;
-
-    var L2 = ( this.end - this.rescue ) + ',0';
-
-    var L3 = ( this.begin + this.offset * 2 ) + ',0';
-
-    this.begin = this.end;
-    this.tip = this.end - this.offset;
-    this.rescue = this.text_offset;
-    this.offset = 0;
-
-    return [ M, L1, L2, L3 ].join( ' ' );
-}
-
-Badge.prototype.style_14 = function( text )
-{
-    var M = 'M' + this.begin + ',0';
-    this.text_x = this.tip + this.text_offset + this.offset;
-
-    this.end = this.begin + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-    this.max_width = this.end;
-    var L1 = this.end + ',0';
-
-    var L2 = ( this.end - this.rescue ) + ',' + this.char_height;
-
-    var L3 = ( this.begin + this.offset * 2 ) + ',' + this.char_height;
-
-    this.begin = this.end;
-    this.tip = this.end - this.offset;
-    this.rescue = this.text_offset;
-    this.offset = 0;
-
-    return [ M, L1, L2, L3 ].join( ' ' );
-}
-
-Badge.prototype.style_15 = function( text )
-{
-    var r = this.char_height / 2;
-
-    var M = 'M' + this.begin + ',0';
-    this.text_x = this.begin + this.text_offset + this.offset;
-
-    this.end = this.begin + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-    this.max_width = this.end + r;
-    var L1 = this.end + ',0';
-
-    // a10,10 0 0,1 0,20
-    var a1 = 'a' + r + ',' + r + ' 0 0,1 0,' + this.char_height;
-
-    var L2 = 'L' + this.begin + ',' + this.char_height;
-
-    // a10,10 0 1,0 0,-20
-    var a2 = 'a' + r + ',' + r + ' 0 1,0 0,' + ( -this.char_height );
-
-    if( this.delimiter !== 0 && --this.max_suffix )
-    {
-        this.end += this.delimiter;
-    }
-
-    this.begin = this.end;
-
-    return [ M, L1, a1, L2, a2 ].join( ' ' );
-}
-
-Badge.prototype.style_16 = function( text )
-{
-    var r = this.char_height / 2;
-
-    var M = 'M' + ( this.begin + r ) + ',0';
-    this.text_x = this.tip + this.text_offset;
-
-    this.end = this.begin + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-    this.end += r;
-    this.max_width = this.end;
-    var L1 = this.end + ',0';
-
-    // a10,10 0 1,0 0,20
-    var a1 = 'a' + r + ',' + r + ' 0 1,0 0,' + this.char_height;
-
-    var L2 = 'L' + ( this.begin + r ) + ',' + this.char_height;
-
-    // a10,10 0 0,1 0,-20
-    var a2 = 'a' + r + ',' + r + ' 0 0,1 0,' + ( -this.char_height );
-
-    if( this.delimiter !== 0 && --this.max_suffix )
-    {
-        this.end += this.delimiter;
-    }
-
-    this.begin = this.end - r;
-    this.tip = this.end - r ;
-
-    return [ M, L1, a1, L2, a2 ].join( ' ' );
-}
-
-Badge.prototype.style_17 = function( text )
+// (prefix):(suffix)
+Badge.prototype.style_9 = function( text )
 {
     var r = this.char_height / 2;
 
@@ -547,102 +366,7 @@ Badge.prototype.style_17 = function( text )
     return [ M, L1, a1, L2, a2 ].join( ' ' );
 }
 
-Badge.prototype.style_18 = function( text )
-{
-    var r = this.char_height / 2;
-
-    var M = 'M' + this.begin + ',0';
-    this.text_x = this.tip + this.text_offset + this.offset;
-
-    this.end = this.begin + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-    if( text.length !== 0 )
-    {
-        this.end += r;
-    }
-
-    var L1 = this.end + ',0';
-    this.max_width = this.end;
-
-    // a10,10 0 1,0, 0,20
-    var a1 = 'a' + r + ',' + r + ' 0 1,0 0,' + this.char_height;
-
-    var L2 = 'L' + this.begin + ',' + this.char_height;
-
-    // a10,10 0 1,0 0,-20
-    var a2 = 'a' + r + ',' + r + ' 0 1,0 0,' + ( -this.char_height );
-
-    this.max_suffix = this.rescue;
-    if( this.delimiter !== 0 && --this.max_suffix )
-    {
-        this.end += this.delimiter;
-    }
-
-    this.begin = this.end;
-    this.tip = this.end;
-
-    return [ M, L1, a1, L2, a2 ].join( ' ' );
-}
-
-Badge.prototype.style_19 = function( text )
-{
-    var r = this.char_height / 2;
-
-    var M = 'M' + this.begin + ',0';
-    this.text_x = this.tip + this.text_offset + this.offset;
-
-    this.end = this.begin + ( this.text_offset * 2 ) + ( text.length * this.char_width );
-
-    if( text.length !== 0 )
-    {
-        this.end += r;
-    }
-
-    var L1 = this.end + ',0';
-
-    // a10,10 0 1,0, 0,20
-    var a1 = 'a' + r + ',' + r + ' 0 1,0 0,' + this.char_height;
-
-    var L2 = 'L' + this.begin + ',' + this.char_height;
-
-    // a10,10 0 1,0 0,-20
-    var a2 = 'a' + r + ',' + r + ' 0 1,0 0,' + ( -this.char_height );
-
-    this.rescue = this.max_suffix;
-    if( this.delimiter !== 0 && --this.max_suffix )
-    {
-        this.end += this.delimiter;
-    }
-
-    var P = this.end;
-    this.max_suffix = this.rescue;
-    //  d="M110,10 a10,10 0 1,0 0,0.1"
-    if( --this.max_suffix )
-    {
-        var A = 'M' + ( P + r ) + ',' + r;
-        var B = 'a' + r + ',' + r + ' 0 1,0 0,0.1';
-
-        var path = document.createElementNS( this.svgns, 'path' );
-        path.setAttribute( 'fill', '#00E100' );
-        path.setAttribute( 'd', [ A, B ].join( ' ' ) );
-        var svg = doc.class( 'svg' );
-        svg[ svg.length - 1 ].appendChild( path );
-    }
-
-    this.max_suffix = this.rescue;
-    if( this.delimiter !== 0 && --this.max_suffix )
-    {
-        this.end += this.delimiter;
-    }
-    this.max_suffix = this.rescue - 1;
-
-    this.begin = this.end;
-    this.tip = this.end;
-    this.max_width = this.end;
-
-    return [ M, L1, a1, L2, a2 ].join( ' ' );
-}
-
-// style 20 == heart
+// style 10 == heart
 Badge.prototype.style_heart = function( text )
 {
     // prefix and suffix => ps
@@ -761,46 +485,6 @@ Badge.prototype.find_points = function( text )
         return this.style_9( text );
         break;
 
-        case 10:
-        return this.style_10( text );
-        break;
-
-        case 11:
-        return this.style_11( text );
-        break;
-
-        case 12:
-        return this.style_12( text );
-        break;
-
-        case 13:
-        return this.style_13( text );
-        break;
-
-        case 14:
-        return this.style_14( text );
-        break;
-
-        case 15:
-        return this.style_15( text );
-        break;
-
-        case 16:
-        return this.style_16( text );
-        break;
-
-        case 17:
-        return this.style_17( text );
-        break;
-
-        case 18:
-        return this.style_18( text );
-        break;
-
-        case 19:
-        return this.style_19( text );
-        break;
-
         default:
         return this.style_1( text );
         break;
@@ -813,14 +497,13 @@ Badge.prototype.fix_width = function()
     svg[ svg.length - 1 ].setAttribute( 'width', this.max_width );
 }
 
-
 Badge.prototype.text = function( string )
 {
     var text = document.createElementNS( this.svgns, 'text' );
     text.setAttribute( 'fill', this.text_color );
     text.setAttribute( 'x', this.text_x );
     text.setAttribute( 'y', this.font_size - 1 );
-    text.setAttribute( 'font-family', 'DejaVu Sans Mono, Source code variable, monospace' );
+    text.setAttribute( 'font-family', '"DejaVu Sans Mono", "Source code variable", monospace' );
     text.setAttribute( 'font-size', this.font_size );
 
     var contents = document.createTextNode( string );
@@ -919,7 +602,7 @@ Badge.prototype.create = function( array )
     this.title();
     this.desc();
 
-    if( this.style === 20 )
+    if( this.style === 10 )
     {
         this.style_heart( text );
     }
